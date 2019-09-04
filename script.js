475fbc0f329df85e53970735ef9da0c6bc1a27bc
Kirstin Black <kiblack@e1z2r7p1.42.us.org>
2019-09-03 20:19:39 -0700

    Pixel Generator

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const $tableElement = $('#pixelCanvas');
+const $inputHeight = $('#inputHeight');
+const $inputWidth = $('#inputWidth');
+const $colorPicker = $('#colorPicker');
+
+// Used to adjust the size of the Grid. The submit button is 
+// implimented to apply the specified height and weight.
+$('#sizePicker').submit( event => {
+    event.preventDefault();
+
+    let width = $inputWidth.val();
+    let height = $inputHeight.val();
+
+    $tableElement.html(''); //clear
+
+    makeGrid(height, width);
+    addCellClickListener();
+});
+
+function makeGrid(height, width) {
+    for(let i = 0; i < height; i++) {
+        $tableElement.append('<tr></tr>');
+    };
+
+    for(let i = 0; i < width; i++) {
+        $('tr').append('<td></td>');
+    };
+};
+// The function is used to choose the specifed color you want
+// to use for your pixel.
+function addCellClickListener() {
+    $('td').click( event => {
+        let color = $colorPicker.val();
+        $(event.currentTarget).css("background-color", color)
+    });
+};
+
+
+
